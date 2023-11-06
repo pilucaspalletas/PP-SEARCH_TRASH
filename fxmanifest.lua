e310b034d8c77e9ef0b52cc4c2f438f40fc30848
pilucaspalletas <128622471+pilucaspalletas@users.noreply.github.com>
2023-11-06 19:36:56 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'Barikelo'
+
+description 'script to search the garbage 100% configurable'
+ 
+server_scripts { 'server/*.lua' }
+ 
+client_scripts { 'client/*.lua' }
+ 
+shared_scripts { 'config.lua' } 
+client_script "IR.lua"
